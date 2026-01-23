93a99f661c62717d716d74d65c8ec087e3e90776
Varun Varma M <varunvarma867@gmail.com>
2026-01-23 14:27:54 +0530

    Update firebase.js

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -5,12 +5,12 @@ import { getFirestore } from "firebase/firestore";
 // TODO: Replace with your actual Firebase project configuration
 // These should ideally be environment variables in a real production app.
 const firebaseConfig = {
-  apiKey: "AIzaSyAJ6Z4fJa96Y-RgsdUzzVtoqFWDZUTfmAU",
-  authDomain: "agrostock-f420f.firebaseapp.com",
-  projectId: "agrostock-f420f",
-  storageBucket: "agrostock-f420f.firebasestorage.app",
-  messagingSenderId: "102073330816",
-  appId: "1:102073330816:web:59be903608540e859f4a65"
+  apiKey: 123,
+  authDomain: 6678,
+  projectId: 345,
+  storageBucket: 345,
+  messagingSenderId: 789,
+  appId: 5679
 };
 
 const app = initializeApp(firebaseConfig);
